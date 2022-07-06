e0e0f726a0b604bbfa887c39effe2c8944af9da0
OnurPekmezci <onur.pekmezci@gmail.com>
2022-07-06 14:36:20 +0300

    onur main3

--- src/Iaraba.java
+++ src/Iaraba.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public interface Iaraba {
 
     String s ="try";
     int a=0;
+
+    ArrayList<Integer> qqq= new ArrayList();
+    Map ll= new HashMap<>();
 }
